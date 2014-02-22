efd781fe8e59df2b311a046da30009b42bb1cdc5
Sparklehorse <heyulin0416@163.com>
2014-02-23 00:24:33 +0800

    display by words

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-//alert("shut down in 5 seconds");
-
-
+function display(text)
+{
+if(count<text.length)
+{
+count++;
+$("#display").text(text.substring(0,count));
+}
+}
 
 (function main()
 {
-var text;
+count=0;
+//text="123456789";
+
 $.ajax({
 				type: "GET",
 				url: "code.txt",
-				success: function(msg){
-				text=msg;
-				alert(text);
+				success: function(text){
+				display(text);
+				setInterval("display(text)",400); 
 				
 				}
 })
 
-/*
-while(1)
-{
-alert("~~~~"+text);
-}
-*/
 
 })();
