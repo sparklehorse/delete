a6609633292526cc7548bc4215feafad87308a83
Sparklehorse <heyulin0416@163.com>
2014-02-22 14:20:36 +0800

    read text

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-alert("shut down in 5 seconds");
+//alert("shut down in 5 seconds");
 
+
+
+(function main()
+{
+var text;
 $.ajax({
 				type: "GET",
 				url: "code.txt",
-				success: function(msg){//get object
-				alert(msg);
+				success: function(msg){
+				text=msg;
+				alert(text);
+				
 				}
 })
+
+
+alert("~~~~"+text);
+
+})();
