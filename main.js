1bdee6cf890e7e102c63a9cd8b056a31d3466180
Sparklehorse <heyulin0416@163.com>
2014-02-27 15:40:26 +0800

    add again

--- main.js
+++ main.js
@@ -10,30 +10,41 @@ $("#display").append(text.substring(count,count+1));
 }
 count++;
 }
+
 function setCookie()
 {
 alert("set cookie")
 document.cookie="viewed=yes";
 //getCookie();
 }
+
 function getCookie()
 {
 var cookie=document.cookie;
 if(cookie=="")
 {
-alert("no cookie");
+//alert("no cookie");
+return 0;
 }
 else
 {
-alert(cookie);
+//alert(cookie);
+return 1;
 }
+}
+
+function deleteCookie()
+{
+document.cookie="";
 }
 (function main()
 {
 count=0;
 text="123456789";
 
-getCookie();
+var viewed=getCookie();
+if(viewed==0)
+{
 //setInterval("display(text)",100);
 
 $.ajax({
@@ -48,7 +59,30 @@ $.ajax({
 				}
 				
 })
-
+}
+else if(viewed==1)
+{
+$.ajax({
+				type: "GET",
+				url: "A deep-sworn vow_utf8.txt",
+				contentType: "text/html; charset=utf8",
+				success: function(msg){
+				text=msg;
+				$("#display").text(text);
+				//display(text);
+				//setInterval("display(text)",100); 
+				
+				}
+				
+})
+}
 setCookie();
+
+ $(document).ready(function(){
+  	          $("#again").bind("click",function(){ 
+			  display(text);
+				setInterval("display(text)",100);
+			  })
+			  })
 
 })();
